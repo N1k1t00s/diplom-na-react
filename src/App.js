2dd37866ad419db3ecd5f6756b3a28872589b248
N1k1t00s <nikitak2460@yandex.ru>
2023-06-06 05:39:08 +0300

    init: create directories: UI > button, input

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, {useState} from 'react';
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import './styles/App.css';
+import PostList from "./components/PostList";
 
 function App() {
 
-  const [posts, setPosts] = useState()
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Java', body: 'Description'},
+        {id: 2, title: 'Java', body: 'Description'},
+        {id: 3, title: 'Java', body: 'Description'},
+        {id: 4, title: 'Java', body: 'Description'},
+        {id: 5, title: 'Java', body: 'Description'}
+    ])
 
-  return (
-    <div className="App">
-    <PostItem post={{id: 1, title: 'Java', body: 'Description'}}></PostItem>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form>
+                <input type="text" placeholder="Название поста"/>
+                <input type="text" placeholder="Описание поста"/>
+                <button>Создать пост</button>
+            </form>
+            <PostList posts={posts} title="Список постов"/>
+        </div>
+    );
 }
 
 export default App;
