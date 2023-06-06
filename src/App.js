9e5c230b8c02ef46b04c81c94462629705005d17
N1k1t00s <nikitak2460@yandex.ru>
2023-06-06 06:56:27 +0300

    init: create PostForm.jsx

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, {useState} from 'react';
-import Counter from "./components/Counter";
-import PostItem from "./components/PostItem";
 import './styles/App.css';
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Java', body: 'Description'},
-        {id: 2, title: 'Java', body: 'Description'},
-        {id: 3, title: 'Java', body: 'Description'},
-        {id: 4, title: 'Java', body: 'Description'},
-        {id: 5, title: 'Java', body: 'Description'}
-    ])
+    const [posts, setPosts] = useState([])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
 
     return (
         <div className="App">
-            <form>
-                <MyInput type="text" placeholder="Название поста" resize="vertical"/>
-                <MyInput type="text" placeholder="Описание поста"/>
-                <MyButton>Создать пост</MyButton>
-            </form>
+            <PostForm create={createPost}/>
             <PostList posts={posts} title="Список постов"/>
         </div>
     );
