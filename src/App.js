3d12edbcc21a3102012c38dfddd079d8d78f53f6
N1k1t00s <nikitak2460@yandex.ru>
2023-06-06 05:04:27 +0300

    init: create PostItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
 
 function App() {
 
+  const [posts, setPosts] = useState()
+
   return (
     <div className="App">
-
+    <PostItem post={{id: 1, title: 'Java', body: 'Description'}}></PostItem>
     </div>
   );
 }
