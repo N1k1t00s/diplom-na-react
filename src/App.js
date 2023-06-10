3a064f44aaedf57ed5208b303f4542c51989a5ee
N1k1t00s <nikitak2460@yandex.ru>
2023-06-10 15:20:12 +0300

    init: create PostService.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -13,6 +13,15 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+    useEffect(() => {
+        fetchPosts()
+    },[])
+
+    async function fetchPosts(){
+        const posts = await PostService.getAll();
+        setPosts(posts.data);
+    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
